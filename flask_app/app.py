af0a8abcb41c39504b1ffe6f156934ffd9dff8b4
Akshat Sharma <akshatsharma5877@gmail.com>
2025-02-28 13:35:11 +0530

    add docker workflow

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,13 @@
 from flask import Flask,render_template,request
 import mlflow
-from flask_app.preprocessing_utility import normalize_text
-import dagshub
 import pickle
 import os
+import re
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import numpy as np
 
 # Set up DagsHub credentials for MLflow tracking
 dagshub_token = os.getenv("AKSHAT")
@@ -16,6 +20,66 @@ os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 dagshub_url = "https://dagshub.com"
 repo_owner = "akshatsharma2407"
 repo_name = "practice"
+
+
+
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+def lemmatization(text):
+    """Lemmatize the text."""
+    lemmatizer = WordNetLemmatizer()
+    text = text.split()
+    text = [lemmatizer.lemmatize(word) for word in text]
+    return " ".join(text)
+
+def remove_stop_words(text):
+    """Remove stop words from the text."""
+    stop_words = set(stopwords.words("english"))
+    text = [word for word in str(text).split() if word not in stop_words]
+    return " ".join(text)
+
+def removing_numbers(text):
+    """Remove numbers from the text."""
+    text = ''.join([char for char in text if not char.isdigit()])
+    return text
+
+def lower_case(text):
+    """Convert text to lower case."""
+    text = text.split()
+    text = [word.lower() for word in text]
+    return " ".join(text)
+
+def removing_punctuations(text):
+    """Remove punctuations from the text."""
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = text.replace('؛', "")
+    text = re.sub('\s+', ' ', text).strip()
+    return text
+
+def removing_urls(text):
+    """Remove URLs from the text."""
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    return url_pattern.sub(r'', text)
+
+def remove_small_sentences(df):
+    """Remove sentences with less than 3 words."""
+    for i in range(len(df)):
+        if len(df.text.iloc[i].split()) < 3:
+            df.text.iloc[i] = np.nan
+
+def normalize_text(text):
+    """Normalize the text data."""
+    try:
+        text = lower_case(text)
+        text = remove_stop_words(text)
+        text = removing_numbers(text)
+        text = removing_punctuations(text)
+        text = removing_urls(text)
+        text = lemmatization(text)
+        return text
+    except Exception as e:
+        raise
 
 mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
@@ -62,4 +126,4 @@ def predict():
         return 'happy'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
