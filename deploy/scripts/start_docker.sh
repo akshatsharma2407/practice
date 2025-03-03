f3c4c6cfc09bd71562a49397177529035eb01b65
Akshat Sharma <akshatsharma5877@gmail.com>
2025-03-03 22:18:19 +0530

    corrected image version

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -3,7 +3,7 @@
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 851725541946.dkr.ecr.ap-southeast-2.amazonaws.com
 
 # Pull the latest image
-docker pull 851725541946.dkr.ecr.ap-southeast-2.amazonaws.com/emotion1:v3
+docker pull 851725541946.dkr.ecr.ap-southeast-2.amazonaws.com/emotion1:latest
 
 if [ "$(docker ps -q -f name=campusx-app)" ]; then
     # Stop the running container
@@ -17,4 +17,4 @@ if [ "$(docker ps -aq -f name=campusx-app)" ]; then
 fi
 
 # Run a new container
-docker run -d -p 80:5000 -e AKSHAT=8a0f419a615e6eae881208edae20992709002635 --name campusx-app 851725541946.dkr.ecr.ap-southeast-2.amazonaws.com/emotion1:v3
+docker run -d -p 80:5000 -e AKSHAT=8a0f419a615e6eae881208edae20992709002635 --name campusx-app 851725541946.dkr.ecr.ap-southeast-2.amazonaws.com/emotion1:latest
