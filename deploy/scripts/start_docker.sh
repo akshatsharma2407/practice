f912e0d5f96239747857b18321bbbfc43d64bb29
Akshat Sharma <akshatsharma5877@gmail.com>
2025-03-03 16:37:20 +0530

    rolling deployment

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -3,10 +3,18 @@
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 851725541946.dkr.ecr.ap-southeast-2.amazonaws.com
 
 # Pull the latest image
-docker pull 851725541946.dkr.ecr.ap-southeast-2.amazonaws.com/emotion1:v3
+docker pull 851725541946.dkr.ecr.ap-southeast-2.amazonaws.com/emotion1:v4
 
-docker stop campusx-app || true
-docker rm campusx-app || true
+if [ "$(docker ps -q -f name=campusx-app)" ]; then
+    # Stop the running container
+    docker stop campusx-app
+fi
+
+# Check if the container 'campusx-app' exists (stopped or running)
+if [ "$(docker ps -aq -f name=campusx-app)" ]; then
+    # Remove the container if it exists
+    docker rm campusx-app
+fi
 
 # Run a new container
-docker run -d -p 80:5000 -e AKSHAT=8a0f419a615e6eae881208edae20992709002635 --name campusx-app 851725541946.dkr.ecr.ap-southeast-2.amazonaws.com/emotion1:v3
+docker run -d -p 80:5000 -e AKSHAT=8a0f419a615e6eae881208edae20992709002635 --name campusx-app 851725541946.dkr.ecr.ap-southeast-2.amazonaws.com/emotion1:v4
